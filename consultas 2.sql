ffab6ce75657d4b51be4895cf2cfebe1865128d1
JUANSPORTELA <juansebastianportelaprado@gimnasiocalibio.edu.co>
2023-03-02 12:24:15 -0500

    consultas 2

---
+++ consultas 2.sql
@@ -0,0 +1,49 @@
+Trabajo de sistemas
+P1
+MAs tiomas
+LISTA D SELECT Nombre, Apellido
+18) - Chr. CHEQUEO FROM tb/Usuario
+Where Saldo =Max(saldo);
+P2
+SELECT Nombre, Apellido
+PDF
+FROM ilUsuario
+FORMUL
+Where Saldo =Min(saldo);
+MEDIC: 
+P3
+SELECT Usuario, Saldo
+FROM tblU suario
+Where Genero ="H" and Saldo =Max(saldo);
+P4
+HG
+SELECT Nombre,Apellido, / eletono
+FROM thIUsuario
+Where Marca= NOKIA;
+P5
+[SELECT Nombre, Apellido, Telefono
+ORATO FROM IbIUsuario
+Where Saldo =>300;
+P6
+SELECT Usuanos
+FROM tu suano
+Where Nombre =*a*
+P7
+SELECT count (
+FROM tolUsuaro
+Where Saldo=> 300;
+P8
+PDF
+HE
+SELECT sum(Saldo)
+FROM tbIUsuaro
+Where Compania="MOVISTAR";
+P9
+SELECT count(*)
+PDF
+FROM tbiUsuario
+DRATO Group by Marca;
+P10
+SELECT count (*)
+FROM tblUsuario
+Group by Nivel;
